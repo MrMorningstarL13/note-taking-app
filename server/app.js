24daf9e03ec3bf2e314dde1e80958f793b10226b
Ion Alexandru <ion.1alexandruleonard@yahoo.ro>
2026-01-15 14:13:46 +0200

    express initialization & firestore connection

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+require('dotenv').config();
+const db = require('./db');
+
+const app = express();
+const port = process.env.PORT || 8888;
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url} ${res.statusCode}`);
+    next();
+})
+
+app.get('/', async (req, res) => {
+    const users = await db.collection('users').get();
+    res.json(users.docs.map(doc => doc.data()));
+});
+
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`)
+})
---
+++ server/db.js
@@ -0,0 +1,14 @@
+const admin = require('firebase-admin');
+
+function initializeFirestore() {
+
+    admin.initializeApp({
+        credential: admin.credential.cert(require('./service_account_key.json'))
+    });
+
+    return admin.firestore();
+}
+
+const db = initializeFirestore();
+
+module.exports = db;
